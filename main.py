688993f86df0592128c9fef812d144e959a9759c
GolubevVA <v.golubev2004@gmail.com>
2025-05-18 14:25:11 +0300

    pytest setup

---
+++ main.py
@@ -0,0 +1,7 @@
+from pkg.utils.sample import get42
+
+def main():
+	print(get42())
+
+if __name__ == "__main__":
+	main()
---
+++ tests/test_example.py
@@ -0,0 +1,4 @@
+from pkg.utils.sample import get42
+
+def test_get42():
+    assert get42() == 42
