9417bc2493fac6b0459ed276d2efcf04ab49650a
Dmitry Rempel <drempel@internet.ru>
2025-06-17 03:42:07 +0300

    Feat: implement sender, add tests

--- pkg/notification/pushy_api.py
+++ pkg/notification/pushy_api.py
@@ -1,4 +1,7 @@
+import aiohttp
+
 from config import PushyConfig
+from pkg.scopus import Article
 
 DEFAULT_ENDPOINT = "https://api.pushy.tg"
 
@@ -7,5 +10,25 @@ class NotificationService:
     def __init__(self, config: PushyConfig):
         self.config = config
 
-    def send_notifications(self, notifications):
-        print("Sending notifications...")
+    async def send_notifications(self, articles: dict[str, list[Article]]):
+        for lang, articles in articles.items():
+            if len(articles) == 0:
+                continue
+            message = f"Новые статьи для языка <b>{lang}</b>:\n\n"
+            for article in articles:
+                message += f"{self._article_to_str(article)}\n\n"
+            await self._send_message(message, lang)
+
+    async def _send_message(self, text: str, lang: str):
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(f"{DEFAULT_ENDPOINT}/v1/feeds/{self.config.feed}/text",
+                                        params={"tags": lang, "mode": "html"}, data=text) as post:
+                    await post.json()
+        except Exception as ignored:
+            pass
+
+    @classmethod
+    def _article_to_str(cls, article: Article) -> str:
+        return (f'<b><a href="{article.scopus_link}">{article.title}</a></b>\n'
+                f"<b>Автор:</b> {article.creator}\n")
---
+++ tests/pushy/test_pushy.py
@@ -0,0 +1,104 @@
+import os
+
+import pytest
+
+from config import PushyConfig
+from pkg.notification.pushy_api import NotificationService
+from pkg.scopus import Article
+
+
+def get_article() -> Article:
+    data = {
+        '@_fa': 'true',
+        'link': [
+            {
+                '@_fa': 'true',
+                '@ref': 'self',
+                '@href': 'https://api.elsevier.com/content/abstract/scopus_id/105005016947'
+            }, {
+                '@_fa': 'true',
+                '@ref': 'author-affiliation',
+                '@href': 'https://api.elsevier.com/content/abstract/scopus_id/105005016947?field=author,affiliation'
+            }, {
+                '@_fa': 'true',
+                '@ref': 'scopus',
+                '@href': 'https://www.scopus.com/inward/record.uri?partnerID=HzOxMe3b&scp=105005016947&origin=inward'
+            }, {
+                '@_fa': 'true',
+                '@ref': 'scopus-citedby',
+                '@href': 'https://www.scopus.com/inward/citedby.uri?partnerID=HzOxMe3b&scp=105005016947&origin=inward'
+            }, {
+                '@_fa': 'true',
+                '@ref': 'full-text',
+                '@href': 'https://api.elsevier.com/content/article/eid/1-s2.0-S2666038520000122'
+            }
+        ],
+        'prism:url': 'https://api.elsevier.com/content/abstract/scopus_id/105005016947',
+        'dc:identifier': 'SCOPUS_ID:105005016947',
+        'eid': '2-s2.0-105005016947',
+        'dc:title': 'In the Case of Theme: Topic Identifiers in English and Norwegian Academic Texts',
+        'dc:creator': 'Hasselgård H.',
+        'prism:publicationName': 'Contrastive Pragmatics',
+        'prism:issn': '26660385',
+        'prism:eIssn': '26660393',
+        'prism:volume': '1',
+        'prism:issueIdentifier': '1',
+        'prism:pageRange': '108-135',
+        'prism:coverDate': '2020-01-01',
+        'prism:coverDisplayDate': '2020',
+        'prism:doi': '10.1163/26660393-12340001',
+        'citedby-count': '0',
+        'affiliation': [
+            {
+                '@_fa': 'true',
+                'affilname': 'Universitetet i Oslo',
+                'affiliation-city': 'Oslo',
+                'affiliation-country': 'Norway'
+            }
+        ],
+        'prism:aggregationType': 'Journal',
+        'subtype': 'ar',
+        'subtypeDescription': 'Article',
+        'source-id': '21101077304',
+        'openaccess': '1',
+        'openaccessFlag': True,
+        'freetoread': {
+            'value': [
+                {
+                    '$': 'all'
+                },
+                {
+                    '$': 'publisherfullgold'
+                },
+                {
+                    '$': 'repository'
+                },
+                {
+                    '$': 'repositoryvor'
+                }
+            ]
+        },
+        'freetoreadLabel': {
+            'value': [
+                {
+                    '$': 'All Open Access'
+                },
+                {
+                    '$': 'Gold'
+                },
+                {
+                    '$': 'Green'
+                }
+            ]
+        }
+    }
+
+    return Article.model_validate(data)
+
+
+async def test_pushy_send():
+    key = os.getenv("PUSHY_FEED")
+    if not key:
+        pytest.skip("PUSHY_FEED not set in environment")
+    notification_service = NotificationService(config=PushyConfig(feed=key))
+    await notification_service.send_notifications({"ru": [get_article()]})
